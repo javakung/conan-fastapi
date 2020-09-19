ba724cd4154a9208de48304ac98208b8abb1b10f
Chatchawal Sangkeettrakarn <conan@conan.in.th>
2020-09-19 18:00:49 +0700

    add function add and mul

--- main.py
+++ main.py
@@ -11,5 +11,14 @@ async def main():
 async def test():
     return 'Test Tutorial'
 
+@app.get("/add")
+async def add(a: int = 0, b: int = 0):
+    return a+b
+
+@app.get("/mul")
+async def mul(a: int = 0, b: int = 0):
+    return a*b
+
+
 if __name__ == '__main__':
    uvicorn.run(app, host="0.0.0.0", port=80, debug=True) 
