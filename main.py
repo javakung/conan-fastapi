ab364ad428623f5bd8e65be8c15060e577ff8112
Chatchawal Sangkeettrakarn <conan@conan.in.th>
2020-09-19 18:57:23 +0700

    clean code

--- main.py
+++ main.py
@@ -21,26 +21,26 @@ async def mul(a: int = 0, b: int = 0):
 
 @app.get("/ctzid-validation")
 async def ctzIdValidate(ctzid):
-  if(len(ctzid) != 13): # ถ้า pid ไม่ใช่ 13 ให้คืนค่า False
-    return False
-  num=0 # ค่าสำหรับอ้างอิง index list ข้อมูลบัตรประชาชน
-  num2=13 # ค่าประจำหลัก
-  listdata=list(ctzid) # list ข้อมูลบัตรประชาชน
-  sum=0 # ผลลัพธ์
-  while num<12:
-    sum+=int(listdata[num])*(num2-num) # นำค่า num เป็น index list แต่ละตัว * (num2 - num) แล้วรวมเข้ากับ sum
-    num+=1 # เพิ่มค่า num อีก 1
-  digit13 = sum%11 # sum หาร 11 เอาเศษ
-  if digit13==0: # ถ้าเศษ = 0
-    digit13=1 # ค่าหลักที่ 13 คือ 1
-  elif digit13==1: # ถ้าเศษ = 1
-    digit13=0 # ค่าหลักที่ 13 คือ 0
-  else:
-    digit13=11-digit13 # ถ้าเศษไม่ใช่กับอะไร ให้เอา 11 - digit13
-  if digit13==int(listdata[12]): # ถ้าค่าหลักที่ 13 เท่ากับค่าหลักที่ 13 ที่ป้อนข้อมูลมา คืนค่า True
-    return True
-  else: # ถ้าค่าหลักที่ 13 ไม่เท่ากับค่าหลักที่ 13 ที่ป้อนข้อมูลมา คืนค่า False
-    return False
+    if(len(ctzid) != 13):
+        return False
+    num=0 
+    sum=0
+    num2=13
+    listdata=list(ctzid)
+    while num<12:
+        sum+=int(listdata[num])*(num2-num)
+        num+=1
+    digit13 = sum%11
+    if digit13==0:
+        digit13=1
+    elif digit13==1:
+        digit13=0
+    else:
+        digit13=11-digit13
+    if digit13==int(listdata[12]):
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
